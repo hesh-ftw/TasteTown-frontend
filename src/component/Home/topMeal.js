feb4d27050e82bd9ee73db626e842a108e465113
Chamath Heshan <chamathheshan076@gmail.com>
2025-01-04 23:12:28 +0530

    add image slider in home page

---
+++ src/component/Home/topMeal.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export const topMeals=[
+    {
+        image:"https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_1280.jpg",
+        title:"pizza"
+    },
+    {
+        image:'https://cdn.pixabay.com/photo/2022/07/15/18/12/bacon-poutine-7323673_640.jpg',
+        title:'french fries'
+    },
+    {
+        image:'https://cdn.pixabay.com/photo/2020/06/02/18/10/noodles-5252012_640.jpg',
+        title:'pasta'
+    },
+    {
+        image:'https://cdn.pixabay.com/photo/2020/07/11/08/01/noodle-5393086_640.jpg',
+        title:'noodles'
+    },
+    {
+        image:'https://cdn.pixabay.com/photo/2023/06/20/10/06/cocktail-8076619_640.jpg',
+        title:'drinks'
+    },
+    {
+        image:'https://cdn.pixabay.com/photo/2021/06/04/11/55/burger-6309618_640.jpg',
+        title:'burgers'
+    },
+]
