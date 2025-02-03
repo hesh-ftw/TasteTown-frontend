734a64b639ee7ae0b7002d1bc0e6a8309c413ca9
Chamath Heshan <chamathheshan076@gmail.com>
2025-02-03 22:52:58 +0530

    update navbar

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,45 +1,60 @@
 import React from 'react'
 import SearchIcon from '@mui/icons-material/Search';
-import { Avatar, IconButton } from '@mui/material';
+import { AppBar, Avatar, IconButton } from '@mui/material';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Cart from './Cart/Cart';
+import { useNavigate } from 'react-router-dom';
+import PersonIcon from '@mui/icons-material/Person';
 
 function NavBar() {
+
+
+  const navigate= useNavigate();
+
   return (
-    <div className='px-5 py-0 bg-[grey] lg:px[20] lg:px-10 flex justify-between'>
+
+    <AppBar >
+      <div className='px-5 py-0 bg-[grey] lg:px[20] lg:px-10 flex justify-between '>
       
-      <div className='lg: mr-10 cursor-pointer flex item-center space-x-4'>
-            <p className='font-semibold py-2'>
-                Taste Town
-            </p>
+
+        
+        <div className='lg: mr-10 cursor-pointer flex item-center space-x-4'>
+              <p className='font-semibold py-2'
+                onClick={()=>{navigate("/")}}
+                >
+                  Taste Town
+              </p>
+        </div>
+
+        <div className='flex item-center'>
+              <div className='py-1'>
+                  <IconButton>
+                      <SearchIcon style={{color: 'white', fontSize:"20"}}/>
+                  </IconButton>
+              </div>
+
+              <div>
+                  <IconButton >
+                      <ShoppingCartIcon
+                        onClick={()=>{navigate("/cart")}}
+                        style={{fontSize:"20",color:"white" , marginTop:5}}> 
+                      </ShoppingCartIcon>
+                  </IconButton>
+              
+              </div>
+
+              <div className=''>
+                
+                     {false?  <Avatar onClick={()=>{navigate("/my-profile/")}} className='mt-2 ' style={{width: 25, height: 25,}}> C </Avatar>:
+                     <IconButton onClick={()=>{navigate("/account/register")}}>
+                      <PersonIcon/>
+                     </IconButton>
+                     }                       
+              </div>
+
+        </div>      
       </div>
-
-      <div className='flex item-center'>
-            <div className='py-1'>
-                <IconButton>
-                    <SearchIcon style={{color: 'white', fontSize:"20"}}/>
-                </IconButton>
-            </div>
-
-            <div className=''>
-                <IconButton>
-                     <ShoppingCartIcon style={{fontSize:"20",color:"white" , marginTop:5}}></ShoppingCartIcon>
-                </IconButton>
-            
-            </div>
-
-            <div className=''>
-               <IconButton>
-                    <Avatar style={{width: 25, height: 25}}>
-                    </Avatar> 
-               </IconButton>
-                               
-            </div>
-
-      </div>
-
-      
-      
-    </div>
+    </AppBar>
   )
 }
 
