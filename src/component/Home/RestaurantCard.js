2116aaa8715be580217346d7dc8a53c3c85fc5cd
Chamath Heshan <chamathheshan076@gmail.com>
2025-02-15 23:02:57 +0530

    fetch restaurants and favorite list from backend

--- src/component/Home/RestaurantCard.js
+++ src/component/Home/RestaurantCard.js
@@ -2,14 +2,41 @@ import React from 'react';
 import { Card, IconButton } from '@mui/material'; 
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from "react-router-dom";
+import { addToFavourite, removeFromFavourite } from '../State/Authentication/Action';
+import { isPresentInFavourites } from '../Config/logic';
 
-const RestaurantCard = () => {
+
+const RestaurantCard = ({item}) => {
+  const navigate= useNavigate();
+  const dispatch= useDispatch();
+  const {auth}= useSelector(store=>store);
+  const jwt = localStorage.getItem("jwt")
+
+
+  const handleAddToFav= ()=>{
+  if (!isPresentInFavourites(auth.favourites, item)) {
+   // console.log('check is present or not t/f ',isPresentInFavourites)
+    dispatch(addToFavourite(jwt, item.id)); 
+  } else {
+    dispatch(removeFromFavourite(jwt, item.id)); 
+  }
+  }
+
+  //navigate to the exact restaurant profile after click 
+  const navigateToRestaurant=()=>{
+      navigate(`/restaurant/${item.name}/${item.id}`)
+    
+  }
+
+
   return (
     <Card className="mb-8 mt-8 w-[18rem]" style={{backgroundColor:"grey"}} >
       <div className={`${true ? "cursor-pointer" : "cursor-not-allowed"} relative`}>
         <img
           className="w-full h-[10rem] rounded-t-md object-cover" 
-          src="http://res.cloudinary.com/dcpesbd8q/image/upload/v1707802815/ux3xq93xzfbqhtudigv2.jpg"
+          src={item.images?.[0] || item.image?.[0] || "fallback-image.jpg"}
           alt="Restaurant"
         />
 
@@ -23,14 +50,14 @@ const RestaurantCard = () => {
 
       <div className="p-4 textPart lg:flex w-full justify-between ">
           <div className="space-y-1">
-              <p className="font-semibold text-lg" >Indian Fast Food</p>
+              <p onClick={navigateToRestaurant} className="font-semibold text-lg cursor-pointer" >{item.name || item.title}</p>
               <p className="Otext-gray-500 text-sm">
-              Craving it all? Dive into our global fla ...
+              {item.description}
               </p>
           </div>
           <div>
-              <IconButton>
-                  {true?<FavoriteIcon/>:<FavoriteBorderIcon/>}
+              <IconButton onClick={handleAddToFav}>
+                  {isPresentInFavourites(auth.favourites,item)?<FavoriteIcon/> :  <FavoriteBorderIcon/>}
               </IconButton>
           </div>
       </div>
--- src/component/Profile/Favourites.js
+++ src/component/Profile/Favourites.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import RestaurantCard from '../Home/RestaurantCard';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUser } from '../State/Authentication/Action';
 
 const Favourites = () => {
+
+  const {auth}= useSelector(store=> store)
+  //const { favourites } = useSelector((store) => store.auth || { favourites: [] });
+  const dispatch= useDispatch();
+  useEffect(() => {
+    dispatch(getUser(localStorage.jwt)); // Fetch the latest user profile including favourites
+  }, [dispatch]);
+
   return (
     <div className='overflow-y-auto max-h-[100vh] ' style={{ position: 'relative', zIndex: 1 }}>
       <h1 className="py-5 font-semibold text-xl text-center mt-10 ">
@@ -9,9 +19,11 @@ const Favourites = () => {
       </h1>
       <div className="flex flex-wrap justify-center gap-6 ">  
 
-        {[1, 1, 1, 1].map((item) => (
-          <RestaurantCard/>
-        ))}
+      {auth.favourites && auth.favourites.length > 0 ? (
+  auth.favourites.map((item) => <RestaurantCard key={item.id} item={item} />)
+) : (
+  <p>No favourite restaurants found.</p>
+)}
       </div>
     </div>
   );
