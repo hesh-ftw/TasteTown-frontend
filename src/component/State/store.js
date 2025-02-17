a035c5479baff05dcd05956944a11b1a9e5d45c9
Chamath Heshan <chamathheshan076@gmail.com>
2025-02-17 23:35:53 +0530

    add reducers to redux store

--- src/component/State/store.js
+++ src/component/State/store.js
@@ -1,10 +1,21 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import { authReducer } from "./Authentication/Reducer";
 import { thunk } from "redux-thunk";
+import { restaurantReducer } from "./Restaurant/Reducer";
+import { MenuItemReducer } from "./Menu/Reducer";
+import { cartReducer } from "./Cart/Reducer";
+import { userOrderReducer } from "./Orders/CustomerOrders/Reducer";
+import { restaurantOrderReducer } from "./Orders/RestaurantOrders/Reducer";
 
 //root reducer
 const rootReducer= combineReducers({
     auth: authReducer,
+    restaurant : restaurantReducer,
+    menu : MenuItemReducer,
+    cart: cartReducer,
+    userOrders: userOrderReducer,
+    restaurantOrders: restaurantOrderReducer,
+    
 })
 
 //create the redux store with redux thunk middleware
