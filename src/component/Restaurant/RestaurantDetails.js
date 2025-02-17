a54871f6b3c26551646a6c6e6ddaf965dfaf844c
Chamath Heshan <chamathheshan076@gmail.com>
2025-02-17 23:34:07 +0530

    update restaurant details component

--- src/component/Restaurant/RestaurantDetails.js
+++ src/component/Restaurant/RestaurantDetails.js
@@ -1,8 +1,13 @@
-import React, { useState } from 'react';
+import React, { useDebugValue, useEffect, useState } from 'react';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import { Divider, FormControl, FormControlLabel, Radio, RadioGroup, Typography } from '@mui/material';
 import MenuCard from './MenuCard';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
+import { getRestaurantById } from '../State/Restaurant/Action';
+import Address from '../Profile/Address';
+import { getAllMenuItems } from '../State/Menu/Action';
 
 const foodTypes = [
     { label: "All", value: "all" },
@@ -11,10 +16,30 @@ const foodTypes = [
     { label: "Seasonal", value: "seasonal" }
 ];
 
-const menu=[1,1,1,1,1,1];
+const menu=[1,1];
 
 const RestaurantDetails = () => {
+
+    const navigate = useNavigate();
+    const dispatch =useDispatch();
+    const jwt= localStorage.getItem("jwt");
+    const {auth,restaurant,menu}= useSelector(store=>store)
+    
+
+
+    const {id,name,streetAddress}= useParams();
     const [foodType, setFoodType] = useState("all");
+
+
+    //find the restaurant by id 
+    console.log("restaurant == ", restaurant);
+
+     useEffect(()=>{
+        dispatch(getRestaurantById(jwt,id));
+        dispatch(getAllMenuItems(jwt,id));
+    },[]);
+    
+    console.log("restaurant menu----", menu);
 
     const handleFilter = (e) => {
        //setFoodType(e.target.value);
@@ -24,19 +49,20 @@ const RestaurantDetails = () => {
     return (
         <div className='px-5 lg:px-20'>
             <section>
-                <h3 className='text-gray-400  py-2 mt-10'>Home / Pizza Hut / 1</h3>
+                <h3 className='text-gray-400  py-2 mt-10'></h3>
 
                 <div className="pt-3 pb-5">
-                    <h1 className="text-4xl font-semibold ">Pizza Hut</h1>
+                    <h1 className="text-4xl font-semibold ">{restaurant.restaurant?.name}</h1>
 
-                    <span className="text-gray-400 flex items-center gap-3" style={{ fontSize: 15, maxWidth: 500 }}>
-                        Description of the restaurant. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore.
+                    <span className="text-gray-400 flex items-center gap-3 mt-7 " style={{ fontSize: 15, maxWidth: 500 }}>
+                        {restaurant.restaurant?.description}
+                        description of restaurant description of restaurant description of restaurant description of restaurant description of restaurant 
                     </span>
 
-                    <div className="lg:ml-[700px] lg:mt-[-100px]">
+                    <div className="lg:ml-[680px] lg:mt-[-70px]">
                         <img 
-                            src="https://cdn.pixabay.com/photo/2016/11/18/14/05/brick-wall-1834784_1280.jpg"
+                            src={restaurant.restaurant?.images[0]}
+
                             alt="Restaurant"
                             style={{ maxHeight: 350, maxWidth: 350 }}
                         />
@@ -45,10 +71,10 @@ const RestaurantDetails = () => {
                     {/* Location and Hours */}
                     <div className="mt-[-100px] text-sm text-gray-400 ">
                         <LocationOnIcon className="mr-2 text-gray-600" fontSize="small" />
-                        <span>Colombo 03</span> <br />
+                        <span>{restaurant.restaurant?.address?.streetAddress}</span> <br />
 
                         <CalendarMonthIcon className="mr-2 text-gray-600" fontSize="small" />
-                        <span>Monday to Friday, 8 AM - 7 PM</span>
+                        <span>{restaurant.restaurant?.openingHrs}</span>
                     </div>
                 </div>
             </section>
@@ -79,7 +105,7 @@ const RestaurantDetails = () => {
              
                 
                 <div className="space-y-5 lg:w-[80%] lg:pl-10 m-5" >Menu
-                    {menu.map((item)=> <MenuCard/>)}
+                    {menu.restaurantMenu.map((item)=> <MenuCard item={item}/>)}
                 </div>
               
             </section>
