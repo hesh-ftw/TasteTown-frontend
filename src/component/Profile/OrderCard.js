9d4e9d279e347039d89675f6888a3838c87b4e5c
Chamath Heshan <chamathheshan076@gmail.com>
2025-02-25 22:47:35 +0530

    update order card

--- src/component/Profile/OrderCard.js
+++ src/component/Profile/OrderCard.js
@@ -1,14 +1,42 @@
+import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material';
 import React from 'react';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-const OrderCard = () => {
+const OrderCard = ({item}) => {
   return (
-    <div className="bg-gray-200 p-5 rounded-lg shadow-md w-full " style={{background:'gray'}}>
-      <h2 className="text-lg font-semibold text-black">Order #12345</h2>
-      <p className="text-sm text-gray-600">Status: Shipped</p>
-      <button className="mt-3 bg-blue-500 text-white px-4 py-2 rounded " style={{ background:'white', color:'black'}}>
-        View Details
-      </button>
-    </div>
+    <Accordion style={{backgroundColor:"grey"}}>
+    <AccordionSummary
+      expandIcon={<ExpandMoreIcon />}
+      aria-controls="panel1-content"
+      id="panel1-header"
+    >  
+      <div className="lg:flex items-center justify-between">
+            <div className="lg:flex items-center lg:gap-5">
+                <img
+                className="w-[4rem] h-[4rem]"
+                src={item.items[0]?.food?.images[0]}
+                alt=""
+                />
+                <div className="space-y-1 lg:space-y-5 lg:max-w-2x1">
+                    <p className="font-semibold text-xl">#{item.id}- {item.items[0]?.food.name} <br/>   
+                    </p>
+                </div>
+            </div>
+        </div>
+    
+    <Typography component="span"></Typography>
+    </AccordionSummary>
+    <AccordionDetails>
+      <Typography>
+        Created On : {item.createdAt} <br/>
+        Order Status : {item.orderStatus} <br/>
+        Total Bill : Rs.{(item.totalPrice).toFixed(2)} <br/>
+        
+      
+      </Typography>
+
+    </AccordionDetails>
+  </Accordion>
   );
 };
 
