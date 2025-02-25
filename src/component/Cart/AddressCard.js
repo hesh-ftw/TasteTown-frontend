8effdaf1d440f4d1f52cee24f1b76d396b1a1934
Chamath Heshan <chamathheshan076@gmail.com>
2025-02-25 22:49:20 +0530

    update address card and map data from backend

--- src/component/Cart/AddressCard.js
+++ src/component/Cart/AddressCard.js
@@ -1,25 +1,53 @@
 import { Button, Card } from '@mui/material'
-import React from 'react'
+import React, { useEffect } from 'react'
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import { green } from '@mui/material/colors';
+import { useDispatch, useSelector } from 'react-redux';
+import { store } from '../State/store';
+import { createOrder } from '../State/Orders/CustomerOrders/Action';
+import toast from 'react-hot-toast';
 
-const AddressCard = ({item, showButton, handleSelectAddress}) => {
-   
+const AddressCard = ({item, showButton}) => {
+
+  const dispatch = useDispatch()
+  const {cart}= useSelector(store=>store)
+
+
+  const handleSelectAddress=(item)=>{
+
+    const data={
+          jwt: localStorage.getItem("jwt"),
+          order:{
+            restaurantId: cart.cartItems[0].food?.restaurant.id,
+            deliveryAddress:{
+              postalCode: item.streetAddress,
+              streetAddress:item.postalCode
+            }
+          }
+        }
+        dispatch(createOrder(data))
+        //toast.success("Order Placed Successfully !")
+        console.log('order created with exisitng adddress ', data)
+  }   
   return (
+
+    
 
     <Card className='flex gap-5 w-64 p-5' style={{background:'grey'}}>
       <LocationOnIcon/>
       <div className='space-y-3 text-grey-400'>
         
         <p>
-            cdcdcdcdc cdcdc 253/2c
+            Street Address : {item.streetAddress} <br/>
+            postal code    : {item.postalCode}
         </p>
 
         {showButton && (
-        <Button  style={{background:'white', color:'black'}} 
-        variant="contained" fullWidth onClick={()=> handleSelectAddress(item)}> select</Button>
+        <Button className='item-center'  style={{background:'white', color:'black'}} 
+        variant="contained" fullWidth onClick={()=> handleSelectAddress(item)}> Delever Here</Button>
         )}
         </div>
+        
     </Card>
    
   )
--- src/component/Cart/addNewAddress.js
+++ src/component/Cart/addNewAddress.js
@@ -1,7 +1,114 @@
+// import * as React from 'react';
+// import Box from '@mui/material/Box';
+// import TextField from '@mui/material/TextField';
+// import { Button } from '@mui/material';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { store } from '../State/store';
+// import { createOrder } from '../State/Orders/CustomerOrders/Action';
+
+// export default function AddNewAddress() {
+//   const [formValues, setFormValues] = React.useState({
+//     street: '',
+//     postalCode: ''
+//   });
+
+//   const handleChange = (event) => {
+//     setFormValues({ ...formValues, [event.target.name]: event.target.value });
+//   };
+
+//   // const handleSubmit = (event) => {
+//   //   event.preventDefault(); // Prevents page reload
+//   //   console.log('Form submitted with values:', formValues);
+//   // };
+
+
+//   const {cart,auth} = useSelector(store=>store)
+//   const dispatch= useDispatch()
+  
+//     const handleSubmitAddress=()=>{
+//       const data={
+//         jwt: localStorage.getItem("jwt"),
+//         order:{
+//           restaurantId: cart.cartItems[0].food?.restaurant.id,
+//           deliveryAddress:{
+      
+//             postalCode: formValues.postalCode,
+//             streetAddress:formValues.street
+//           }
+//         }
+//       }
+//       dispatch(createOrder(data))
+//       console.log("address submit form values :", data)
+//     }
+
+
+//   return (
+//     <Box
+//       component="form"
+//       sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
+//       noValidate
+//       autoComplete="off"
+//       onSubmit={handleSubmitAddress} // Attach submit handler
+//     >
+//       <h1 className="text-white text-lg font-semibold mb-3">Add New Address</h1>
+
+//       <div>
+//         <TextField
+//           label="Street Address"
+//           name="street"
+//           value={formValues.street}
+//           onChange={handleChange}
+//           placeholder="Enter street"
+//           size="small"
+//           InputLabelProps={{ style: { color: 'white' } }} // Label color
+//           inputProps={{ style: { color: 'white' } }} // Input text color
+//           sx={{
+//             '& .MuiOutlinedInput-root': {
+//               '& fieldset': { borderColor: 'white' }, // Border color
+//               '&:hover fieldset': { borderColor: 'white' }, // Hover effect
+//               '&.Mui-focused fieldset': { borderColor: 'white' }, // Focus effect
+//             },
+//           }}
+//         />
+
+//         <TextField
+//           label="Postal Code"
+//           name="postalCode"
+//           value={formValues.postalCode}
+//           onChange={handleChange}
+//           placeholder="Enter postal code"
+//           size="small"
+//           InputLabelProps={{ style: { color: 'white' } }} // Label color
+//           inputProps={{ style: { color: 'white' } }} // Input text color
+//           sx={{
+//             '& .MuiOutlinedInput-root': {
+//               '& fieldset': { borderColor: 'white' }, // Border color
+//               '&:hover fieldset': { borderColor: 'white' }, // Hover effect
+//               '&.Mui-focused fieldset': { borderColor: 'white' }, // Focus effect
+//             },
+//           }}
+//         />
+
+//         <Button 
+//         onSubmit={handleSubmitAddress}
+//           type="submit" 
+//           variant="contained" 
+//           sx={{ backgroundColor: 'gray', color: 'white', mt: 1, ml:1 }} 
+//         > 
+//           DELIVER HERE 
+//         </Button>
+//       </div>
+//     </Box>
+//   );
+// }
+
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Button } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { createOrder } from '../State/Orders/CustomerOrders/Action';
+import toast from 'react-hot-toast';
 
 export default function AddNewAddress() {
   const [formValues, setFormValues] = React.useState({
@@ -13,9 +120,30 @@ export default function AddNewAddress() {
     setFormValues({ ...formValues, [event.target.name]: event.target.value });
   };
 
-  const handleSubmit = (event) => {
+  const { cart } = useSelector(store => store);
+  const dispatch = useDispatch();
+
+  const handleSubmitAddress = (event) => {
     event.preventDefault(); // Prevents page reload
-    console.log('Form submitted with values:', formValues);
+
+    if (!cart.cartItems || cart.cartItems.length === 0) {
+      console.error("Cart is empty. Cannot place order.");
+      return;
+    }
+
+    const data = {
+      jwt: localStorage.getItem("jwt"),
+      order: {
+        restaurantId: cart.cartItems[0].food?.restaurant?.id,
+        deliveryAddress: {
+          postalCode: formValues.postalCode,
+          streetAddress: formValues.street
+        }
+      }
+    };
+
+   dispatch(createOrder(data));
+    console.log("Address submitted with values:", data);
   };
 
   return (
@@ -24,7 +152,7 @@ export default function AddNewAddress() {
       sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
       noValidate
       autoComplete="off"
-      onSubmit={handleSubmit} // Attach submit handler
+      onSubmit={handleSubmitAddress} // Attach submit handler
     >
       <h1 className="text-white text-lg font-semibold mb-3">Add New Address</h1>
 
@@ -65,14 +193,15 @@ export default function AddNewAddress() {
           }}
         />
 
-        <Button 
+        <Button          
           type="submit" 
           variant="contained" 
-          sx={{ backgroundColor: 'gray', color: 'white', mt: 1, ml:1 }} 
+          sx={{ backgroundColor: 'gray', color: 'white', mt: 1, ml: 1 }} 
         > 
-          Submit 
+          DELIVER HERE 
         </Button>
       </div>
     </Box>
   );
 }
+
