0e5bc8f3160f3a6a5f25866542332792caede965
Chamath Heshan <chamathheshan076@gmail.com>
2025-02-25 22:45:08 +0530

    fetch restaurant menu details from backend

--- src/component/Restaurant/MenuCard.js
+++ src/component/Restaurant/MenuCard.js
@@ -1,10 +1,33 @@
 import { Accordion, AccordionDetails, AccordionSummary, Button, Typography } from '@mui/material'
 import React from 'react'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { useDispatch } from 'react-redux';
+import { addItemTocart } from '../State/Cart/Action';
 
-const MenuCard = () => {
+const MenuCard = ({item}) => {
+
+console.log("menu items", item)
+const dispatch = useDispatch()
+
+
+const handleAddItemToCart=()=>{
+  const reqData={
+    jwt: localStorage.getItem("jwt"),
+    cartItem: {
+      foodId: item.id,
+      quantity: 1,      
+    },
+  };
+  console.log("req data", reqData );
+
+  dispatch(addItemTocart(reqData));
+
+}
+
+
+
   return (
-    <Accordion defaultExpanded style={{backgroundColor:"grey"}}>
+    <Accordion style={{backgroundColor:"grey"}}>
     <AccordionSummary
       expandIcon={<ExpandMoreIcon />}
       aria-controls="panel1-content"
@@ -14,12 +37,12 @@ const MenuCard = () => {
             <div className="lg:flex items-center lg:gap-5">
                 <img
                 className="w-[7rem] h-[7rem] object-cover"
-                src="https://cdn.pixabay.com/photo/2016/11/20/09/06/bowl-1842294_640.jpg"
+                src={item.images[0]}
                 alt=""
                 />
                 <div className="space-y-1 lg:space-y-5 lg:max-w-2x1">
-                    <p className="font-semibold text-xl">Burger</p>
-                    <p>Rs.499</p>
+                    <p className="font-semibold text-xl">{item.name}</p>
+                    <p>{item.price}</p>
                 </div>
             </div>
         </div>
@@ -28,12 +51,11 @@ const MenuCard = () => {
     </AccordionSummary>
     <AccordionDetails>
       <Typography>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-        malesuada lacus ex, sit amet blandit leo lobortis eget.
+        {item.description}
       </Typography>
 
       <div className="pt-5">
-            <Button style={{backgroundColor:'black'}} variant="contained" disabled={false} type="submit">
+            <Button onClick={handleAddItemToCart} style={{backgroundColor:'black'}} variant="contained" disabled={false} type="submit">
               {true ? "Add to Cart" : "Out Of Stock"}
             </Button>
           </div>
